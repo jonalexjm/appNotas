530b4f7344d9fe805bba2b2d8df19e4a1b589d2b
alex <jonalexjm@hotmail.com>
2016-09-13 23:12:39 -0500

    v2 formulario adicionar

---
+++ app/src/main/java/jonalexjm/com/appnotas/AdicionarNota.java
@@ -0,0 +1,25 @@
+package jonalexjm.com.appnotas;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class AdicionarNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_nota);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
