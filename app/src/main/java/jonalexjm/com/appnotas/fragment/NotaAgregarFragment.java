2079cd2255ea0d61869fb720721bbaff77f0582a
alex <jonalexjm@hotmail.com>
2016-09-18 22:32:34 -0500

    v5 agregando el formulario de adicionar nota

--- app/src/main/java/jonalexjm/com/appnotas/fragment/NotaAgregarFragment.java
+++ app/src/main/java/jonalexjm/com/appnotas/fragment/NotaAgregarFragment.java
@@ -3,9 +3,13 @@ package jonalexjm.com.appnotas.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import jonalexjm.com.appnotas.R;
 
@@ -15,6 +19,11 @@ import jonalexjm.com.appnotas.R;
  */
 public class NotaAgregarFragment extends Fragment {
 
+    private Button btnBuscarFechas, btnGuardarNota;
+    private TextView txtTitulo, txtContenido, txtFecha, txtDia, txtMes, txtAnno;
+    private EditText edTxtTitulo,edTxtContenido;
+
+
 
     public NotaAgregarFragment() {
         // Required empty public constructor
@@ -22,10 +31,26 @@ public class NotaAgregarFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nota, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_nota, container, false);
+
+        btnBuscarFechas = (Button) v.findViewById(R.id.btnEscoger_fecha);
+        btnGuardarNota = (Button)  v.findViewById(R.id.btnAgregarNota);
+
+        txtTitulo = (TextView) v.findViewById(R.id.txtTitulo);
+        txtContenido = (TextView) v.findViewById(R.id.txtContenido);
+        txtFecha = (TextView) v.findViewById(R.id.txtFecha);
+        txtDia = (TextView) v.findViewById(R.id.txtDia);
+        txtMes = (TextView) v.findViewById(R.id.txtMes);
+        txtAnno = (TextView) v.findViewById(R.id.txtAnno);
+
+        edTxtTitulo = (EditText) v.findViewById(R.id.edTxtTitulo);
+        edTxtContenido = (EditText) v.findViewById(R.id.edTxtContenido);
+
+
+        return v;
     }
 
 }
